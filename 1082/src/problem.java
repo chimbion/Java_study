849388accfae7d34a3e4c7793ebc3febe8497072
chimbion <vadimb@mail.ru>
2017-03-09 12:59:50 +0300

    1082

---
+++ 1082/src/problem.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.StreamTokenizer;
+import java.io.Writer;
+
+public class problem {
+
+   public static void main(String[] args) throws IOException
+   {
+      new problem().run();
+   }
+
+   StreamTokenizer in;
+   PrintWriter out;
+
+   int nextInt() throws IOException
+   {
+      in.nextToken();
+      return (int)in.nval;
+   }
+
+   void run() throws IOException
+   {
+	  boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	  Reader reader = oj ? new InputStreamReader(System.in) : new FileReader("input.txt");
+	  Writer writer = oj ? new OutputStreamWriter(System.out) : new FileWriter("output.txt");
+	  in = new StreamTokenizer(new BufferedReader(reader));
+	  out = new PrintWriter(writer);
+	  solve();
+      out.flush();
+   }
+
+   
+   void solve() throws IOException
+   {
+      int n = nextInt();
+      for (int i = 1; i <= n; i++)
+      {
+ 		  out.print(i);
+  		  out.print(" ");
+      } 
+   }
+
+}
